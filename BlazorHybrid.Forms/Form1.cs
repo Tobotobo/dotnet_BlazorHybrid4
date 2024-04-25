9d57b887e9929a33023bbd688109ba503ca2aabc
Tobotobo <tobotobo.channel@gmail.com>
2024-04-25 22:38:28 +0900

    初回

---
+++ BlazorHybrid.Forms/Form1.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorHybrid.Forms;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+
+        var blazorWebView1 = new BlazorWebView();
+        blazorWebView1.Dock = DockStyle.Fill;
+        this.Controls.Add(blazorWebView1);
+
+        var services = new ServiceCollection();
+        services.AddWindowsFormsBlazorWebView();
+        blazorWebView1.HostPage = "wwwroot\\index.html";
+        blazorWebView1.Services = services.BuildServiceProvider();
+        blazorWebView1.RootComponents.Add<BlazorHybrid.View.Components.Routes>("#app");
+    }
+}
